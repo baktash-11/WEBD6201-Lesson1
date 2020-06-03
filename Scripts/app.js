fa05ced348b17143fac5d6d2845ebd12eaee44c9
Baktash Sana <baktash11@gmail.com>
2020-06-03 10:40:22 -0400

    added bootstrap and font-awesome

--- Scripts/app.js
+++ Scripts/app.js
@@ -5,6 +5,26 @@
     function Start()
     {
         console.log("App Started....");
+
+        let fristHeading = document.getElementById("fristHeading");
+        fristHeading.textContent = "Goodby Crule World!"
+
+        //document.getElementById("fristHeading").textContent="Ya Allah help me!"
+
+        fristHeading.style.fontWeight="bold";
+
+        //getElementsByTagName will return a raay and we will get the frist element of the array
+        let main = document.getElementsByTagName("main")[0];
+        
+        //creating html element 
+        //configuring my new element 
+        let newH2 = document.createElement("h2");
+        newH2.setAttribute("class", "Display-5");
+        newH2.textContent = "Alhamdullah We have You"
+        //have to atach it (newH2), as a chiled of element 
+
+        main.appendChild(newH2);
     }    
+
     window.addEventListener("load", Start);
   })();
